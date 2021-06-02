4ca41717451ed66381e4250f374514bd49f3e16f
April <goapril@yahoo.com>
2021-06-02 14:09:08 +0200

    Added className in render function

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,6 +5,21 @@ import EventList from '../EventList';
 import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+
+  test('render list of events', () => {
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+});
+
+/*describe('<App /> component', () => {
   test('render list of events', () => {
     const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
@@ -13,4 +28,4 @@ describe('<App /> component', () => {
     const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
-});
+});*/
