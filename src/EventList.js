226128d4817e0296c7640238ddd17168b3b17e91
April <goapril@yahoo.com>
2021-06-03 12:50:15 +0200

    Removed extra spaces

--- src/EventList.js
+++ src/EventList.js
@@ -4,14 +4,14 @@ import Event from './Event'; // in src/EventList.js
 class EventList extends Component {
   render() {
     const { events } = this.props;
-  return (
-    <ul className="EventList">
-      {events.map(event =>
-        <li key={event.id}>
-          <Event event={event} />
-        </li>
-      )}
-    </ul>
+    return (
+      <ul className="EventList">
+        {events.map(event =>
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        )}
+      </ul>
     );
   }
 }
