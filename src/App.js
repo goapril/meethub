8041664223d9c2bfbff16f855f6629198e64d7e3
April <goapril@yahoo.com>
2021-06-03 12:50:57 +0200

    Imported NumberOfEvents and added states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: []
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <CitySearch locations={this.state.locations} />
+        <EventList events={this.state.events} />
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} />
+      </div>
+    );
+  }
 }
 
 export default App;
