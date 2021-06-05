4b9945c76f70da83e70763e9350592061b25558a
April <goapril@yahoo.com>
2021-06-05 11:58:47 +0200

    Created new method in “App.js”, named it updateEvents

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { getEvents } from './api';
 
 class App extends Component {
   state = {
@@ -11,10 +12,19 @@ class App extends Component {
     numberOfEvents: []
   }
 
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }  
+
   render() {
     return (
       <div className='App'>
-        <CitySearch locations={this.state.locations} />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} />
       </div>
