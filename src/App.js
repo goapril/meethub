f0c492808fe896dc99161e53e36e56b967c19e29
April <goapril@yahoo.com>
2021-06-06 12:59:45 +0200

    Added headers

--- src/App.js
+++ src/App.js
@@ -40,6 +40,8 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} />
         <EventList events={this.state.events} />
