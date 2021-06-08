60891d37d26b8d3f3c7a075d562cbfb59c171140
April <goapril@yahoo.com>
2021-06-08 12:46:50 +0200

    Wrote scenario 2 test

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { loadFeature, defineFeature } from 'jest-cucumber';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
+import CitySearch from '../CitySearch';
+import { extractLocations } from '../api';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
+const locations = extractLocations(mockData);
 
 defineFeature(feature, test => {
   test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
@@ -23,19 +26,19 @@ defineFeature(feature, test => {
   });
 
   test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+    let CitySearchWrapper;
     given('the main page is open', () => {
-
+      CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}} locations={locations} />);
     });
 
     when('the user starts typing in the city textbox', () => {
-
+      CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
     });
 
     then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-
+      expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
     });
   });
-
 
   test('User can select a city from the suggested list', ({ given, and, when, then }) => {
     given('user was typing “Berlin” in the city textbox', () => {
