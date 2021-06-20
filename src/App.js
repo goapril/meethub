8dd6c28090ecdca51bee856cbe5d59a7a1aa9a6a
April <goapril@yahoo.com>
2021-06-20 10:43:17 +0200

    Changed width to 800

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
         <NumberOfEvents updateEventCount={this.updateEventCount} numberOfEvents={this.state.numberOfEvents} />
         <h4>Events in each city</h4>
          <ScatterChart
-          width={400}
+          width={800}
           height={400}
           margin={{
             top: 20, right: 20, bottom: 20, left: 20,
